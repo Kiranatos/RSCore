c24e89748a662ab377d68be70cbb23eeafde30a3
Darknia Bloody <darkniabloody@gmail.com>
2022-10-03 12:37:49 +0300

    begin

---
+++ Links.rb
@@ -0,0 +1,9 @@
+=begin
+    versions managers: rvm, rbenv
+
+https://github.com/rubocop/ruby-style-guide
+https://www.8host.com/blog/preobrazovanie-tipov-dannyx-v-ruby/
+https://www.techotopia.com/index.php/Ruby_Operator_Precedence#Operator_Precedence_Table
+
+
+=end
